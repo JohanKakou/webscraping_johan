e09a906926a27ca06b1ea843a38538085a86a0af
Johan Kakou <kjdgjohan@gmail.com>
2023-07-08 05:32:47 +0000

    dossier scraper

--- qu_ran/qu_ran/spiders/quran_scraper.py
+++ qu_ran/qu_ran/spiders/quran_scraper.py
@@ -6,4 +6,7 @@ class QuranScraperSpider(scrapy.Spider):
     allowed_domains = ["qurancentral.com"]
     start_urls = ["https://qurancentral.com/fr/"]
     def parse(self, response):       
+        
+        
+        
         pass
